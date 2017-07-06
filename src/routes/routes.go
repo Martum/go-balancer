b3125b175e9b0b2e466de32dbd46c5059f39dc28
Martin Montenegro <martin.92m@gmail.com>
2017-07-05 22:20:56 -0300

    Add servers lists

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -1,11 +1,19 @@
 package routes
 
 import (
+	"fmt"
+
 	"../config"
 )
 
+// Operations
+const GiveMeAServer string = "givemeaserver"
+
 type RouterRequest struct {
-	zarasa string
+	operation string
+	path      string
+	method    string
+	c         *chan RouterResponse
 }
 
 type RouterResponse struct {
@@ -13,5 +21,42 @@ type RouterResponse struct {
 }
 
 func Router(c chan RouterRequest, reglas []config.Regla) {
+	// for {
+	// 	msg := <-c
+	//
+	// 	switch msg.operation {
+	// 	case GiveMeAServer:
+	// 		giveAServer(msg)
+	// 	}
+	// }
+
+	fmt.Println(initColas(reglas))
+}
+
+// Private
+
+type serversLists struct {
+	servers            chan string
+	unavailableServers chan string
+}
+
+func initColas(reglas []config.Regla) map[string]serversLists {
+	serversQueues := make(map[string]serversLists)
+
+	for _, rule := range reglas {
+		cSer := make(chan string, 100)
+		cUnSer := make(chan string, 100)
+
+		for _, server := range rule.Servers {
+			cSer <- server
+		}
+
+		serversQueues[rule.Ruta] = serversLists{servers: cSer, unavailableServers: cUnSer}
+	}
+
+	return serversQueues
+}
+
+func giveAServer(msg RouterRequest) {
 
 }
